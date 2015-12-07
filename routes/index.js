cea75c1cb16e87241a160674be638a13561e5d58
wkwyatt <wkwyatt1@gmail.com>
2015-12-07 17:32:17 -0500

    styling and correct routing for db

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require("mongodb").MongoClient;
-
+var shoesArray = [];
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	MongoClient.connect('mongodb://localhost:27017/test', function(error, db) {
-		db.collection('teams').find().toArray(function(error, result) {
+		db.collection('shoes').find().toArray(function(error, result) {
 			console.log(result);
-			console.log("flag: inside db array");
+			shoesArray = result;
+			console.log("flag: inside db array");  
+			res.render('index', { shoes: shoesArray });
 		});
 	});
 	console.log("flag: inside main route");
@@ -18,11 +20,11 @@ router.get('/', function(req, res, next) {
 	// 4. Load all thos photos into an array
 	// 5. Choose a random image from the array and set it to a var
 	// 6. res.render the index view and send it the photo
-	var serverPhotos = [
-				{name: 'http://chadconway.pbworks.com/f/1253765817/news-electriccar1.jpg'},
-				{name: 'https://c2.staticflickr.com/2/1307/4700132636_cd67861c4b_b.jpg'}		
-		];
-  res.render('index', { photos: serverPhotos });
+	// var serverPhotos = [
+	// 			{name: 'http://chadconway.pbworks.com/f/1253765817/news-electriccar1.jpg'},
+	// 			{name: 'https://c2.staticflickr.com/2/1307/4700132636_cd67861c4b_b.jpg'}		
+	// 	];
+
 });
 console.log("flag: main thread");
 router.get('/standings', function(req, res, next) {
