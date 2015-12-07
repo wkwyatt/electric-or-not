bd4fec910d0f06616217d950299076943c35466b
wkwyatt <wkwyatt1@gmail.com>
2015-12-07 16:28:10 -0500

    init commit for full stack site

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var MongoClient = require("mongodb").MongoClient;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	MongoClient.connect('mongodb://localhost:27017/test', function(error, db) {
+		db.collection('teams').find().toArray(function(error, result) {
+			console.log(result);
+			console.log("flag: inside db array");
+		});
+	});
+	console.log("flag: inside main route");
+	// index page should load a random picture/item
+	// 1. Get all pictures from the MongoDB
+	// 2. Get the current user from the MongoDB via req.id
+	// 3. Find which photos the current user has NOT voted on 
+	// 4. Load all thos photos into an array
+	// 5. Choose a random image from the array and set it to a var
+	// 6. res.render the index view and send it the photo
+	var serverPhotos = [
+				{name: 'http://chadconway.pbworks.com/f/1253765817/news-electriccar1.jpg'},
+				{name: 'https://c2.staticflickr.com/2/1307/4700132636_cd67861c4b_b.jpg'}		
+		];
+  res.render('index', { photos: serverPhotos });
+});
+console.log("flag: main thread");
+router.get('/standings', function(req, res, next) {
+	console.log("flag: inside standing route");
+	// 1. Get all the standings 
+	// 2. Sort by highest likes
+	// 3. res.render the standings view and pass it the sorted photo array
+	res.render('index', {title: 'Standings'});
+});
+
+router.post('*', function(req,res,next) {
+	//this will run for all posted pages
+});
+
+module.exports = router;
